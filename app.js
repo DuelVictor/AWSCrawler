0153f83fa535c780f03b2edddab544788af7e30d
Victor Duel <duel.victor96@gmail.com>
2025-05-06 12:42:38 +0300

    fixedHealth3

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import { router as crawlerRouter } from './routers/crawler.router.js';
-import { router as healthcheckRouter } from './routers/healthcheck.router.js';
 
 export const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("").get("", (req, res) => res.status(200).send('OK'));
+app.route('/').get((req, res) => res.status(200).send('OK'));
 app.use("/api/crawler", crawlerRouter);
