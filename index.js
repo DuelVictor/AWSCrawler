7a1c4c3efb2eda20fdf5dd065db406adae75175f
Victor Duel <duel.victor96@gmail.com>
2025-05-13 17:19:07 +0300

    fixedHost

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const gracefulShutdown = (server) => {
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log('Server is running on port:', PORT);
     signals.forEach(() => gracefulShutdown(server));
 });
